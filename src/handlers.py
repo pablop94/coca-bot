bee56cc0207070f01d48d78499055bc2b4045d11
Pablo Pissi <pablopissi@gmail.com>
2022-01-13 01:41:00 -0300

    Quito decorador de handlers

--- src/handlers.py
+++ src/handlers.py
@@ -4,32 +4,11 @@ import os
 import random
 import traceback
 
+from src.decorators import chat_id_required
 from src.exceptions import NoMealConfigured
 from src.logger import logger
 from src.meals import add_meal, get_next_meal, history, add_history, get_skip, add_skip
 from telegram import ParseMode, Update
-
-
-def get_handler_name(name):
-    parts = name.split("_")
-    return "_".join(parts[: len(parts) - 1])
-
-
-def chat_id_required(fn):
-    fnname = get_handler_name(fn.__name__)
-
-    def inner(update, context):
-        if update.message.chat.id == int(os.environ.get("CHAT_ID")):
-            fn(update, context)
-        else:
-            logger.warning(
-                f"Recibido <{fnname}> desde un chat no configurado: {update.message.chat.id}."
-            )
-            update.message.reply_photo(
-                "https://pbs.twimg.com/media/E8ozthsWQAMproa.jpg"
-            )
-
-    return inner
 
 
 def send_reminder(context):
