45874c93a750eae93f732aa0a69b640be33b8319
Pablo Pissi <pablopissi@gmail.com>
2022-03-13 23:33:49 -0300

    Quito configuracion de DB de settings/base

--- src/coca_sarli/settings/base.py
+++ src/coca_sarli/settings/base.py
@@ -60,17 +60,6 @@ TEMPLATES = [
 WSGI_APPLICATION = "coca_sarli.wsgi.application"
 
 
-# Database
-# https://docs.djangoproject.com/en/4.0/ref/settings/#databases
-
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
-}
-
-
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
 
